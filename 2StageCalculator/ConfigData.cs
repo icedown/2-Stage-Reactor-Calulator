1f6a67c233d62f5e2e8161fd486b4d3ea7673089
IceDown <icedown@gmail.com>
2019-06-19 00:01:13 -0400

    Added license information

--- 2StageCalculator/ConfigData.cs
+++ 2StageCalculator/ConfigData.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  Copyright (C) 2019 Jason Brooks
+//
+//    This file is part of 2 Stage Reactor Calculator.
+//
+//    2 Stage Reactor Calculator is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    2 Stage Reactor Calculator is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with 2 Stage Reactor Calculator.  If not, see<https://www.gnu.org/licenses/>.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
